b89198af352abee6891c328fc72c26c7ce8d7a5c
yuiffydong <yuiffydong@tencent.com>
2018-12-12 18:29:21 +0800

    Updates

---
+++ precache-manifest.092e6a9f05fbbceb1d1dc6c1e29a9cd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e0c9733216bbee3319d",
+    "url": "/static/js/main.2e0c9733.chunk.js"
+  },
+  {
+    "revision": "ef2d399dc1d6cd208305",
+    "url": "/static/js/1.ef2d399d.chunk.js"
+  },
+  {
+    "revision": "2e0c9733216bbee3319d",
+    "url": "/static/css/main.dd4b0e28.chunk.css"
+  },
+  {
+    "revision": "0fa9278a70f20be32dc76a90ca185258",
+    "url": "/index.html"
+  }
+];
